10c97b3cf7dbcb815d6346967c665923fd1cec3b
kiyon21 <kiyonfarokhi@hotmail.com>
2025-03-20 17:06:10 -0400

    started implementing mac vm

---
+++ vm-c/mac/src/data_structures/Stack.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Stack.h"
+
+void push(StackNode **top, int val){
+
+    StackNode *newNode = (StackNode *)malloc(sizeof(StackNode)); // create new node
+
+    if(!newNode){
+        printf("Couldn't malloc for new Node\n");
+        return;
+    }
+    
+    // add new node into stack
+    newNode->data = val;
+    newNode->next = *top;
+    *top = newNode;
+
+}
+
+
+int pop(StackNode **top){
+    if(*top==NULL){
+        printf("Stack UnderFlow\n");
+        return -1;
+    }
+    StackNode *temp = *top;
+    int value = temp->data;
+    *top = (*top)->next;
+
+    free(temp);
+    return value;
+}
+
+int peek(StackNode *top){
+    if(top == NULL) {
+        printf("Stack is Empty\n");
+        return -1; 
+    }
+    else{
+        return top->data;
+    }
+}
+
+int isEmpty(StackNode *top){
+    return top == NULL;
+}
+
+
+
+
---
+++ vm-c/mac/src/data_structures/Stack.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct StackNode {
+	int data;
+	struct StackNode *next;
+} StackNode;
+
+void push(StackNode **top, int val); // push onto stack
+int pop(StackNode **top); // pop from stack
+int peek(StackNode *top); // peek top of stack
+int isEmpty(StackNode *top); // check if stack is empty
+
